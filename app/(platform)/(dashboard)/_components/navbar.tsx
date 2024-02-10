83d3a83453811949259687ce94db1d10cace9f32
Aniket Negi <aniketng08@gmail.com>
2024-02-10 23:34:57 +0530

    In the middle of implementing DB using prisma and supabase(postgresql)

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -2,11 +2,13 @@ import Logo from '@/components/logo';
 import { Button } from '@/components/ui/button';
 import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 import { Plus } from 'lucide-react';
+import { MobileSidebar } from './mobile-sidebar';
 
 export const Navbar = () => {
     return (
         <nav className='fixed px-4 z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center'>
-            <div className='flex items-center gap-x-4'>
+            <MobileSidebar />
+			<div className='flex items-center gap-x-4'>
                 <div className='hidden md:flex'>
                     <Logo />
                 </div>
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,29 @@
-const OrganizationIdPage = () => {
-    return <div>Organization page </div>;
+import { create } from '@/actions/create-board';
+import { Button } from '@/components/ui/button';
+import { db } from '@/lib/db';
+const OrganizationIdPage = async () => {
+    const boards = await db.board.findMany();
+    return (
+        <div className='flex flex-col space-y-4'>
+            <form action={create}>
+                <input
+                    id='title'
+                    placeholder='Enter a board title'
+                    name='title'
+                    required
+                    className='border-black border p-1'
+                />
+            </form>
+            <Button type='submit'>Submit</Button>
+            <div className='space-y-2'>
+                {boards.map((board) => {
+                    return (
+                        <div key={board.id}> Board title: {board.title} </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default OrganizationIdPage;
